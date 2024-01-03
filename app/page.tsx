7333c02fc171e346a1c16f0b0782d0c90538647c
nithinRaj031197 <nithinraj.official3@gmail.com>
2024-01-03 23:28:58 +0530

    Navbar component along with its respective dropdown components

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="relative z-50">
+        <div className="absolute">
+          <Navbar />
+        </div>
+      </div>
+      <div>
+        <div className="overflow-hidden">
+          <div className="relative h-screen">
+            <img
+              className="absolute top-0 transition ease-in duration-200 max-xl:right-[-1050px] xl:right-[-970px] z-30 image"
+              width="4377"
+              src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp"
+              alt="Github Background Image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
